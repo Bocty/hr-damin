b473e23166a960546d24fd4d75aa36a16d3b778c
“Bocty” <2543294624@qq.com>
2022-07-13 11:50:51 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主题上面的header是不是固定定位
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否显示
+   */
+  sidebarLogo: true
+}
